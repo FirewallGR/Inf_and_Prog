136e5acd76ce02fa4b74bd11f8990b5dd5322354
FireCitrus <qwert.ting@bk.ru>
2021-09-24 10:46:40 +0300

    upgrade Task1

--- HWTasks/Task1/main.c
+++ HWTasks/Task1/main.c
@@ -3,8 +3,7 @@
 #include <string.h>
 
 int main() {
-	int x1, x2, y1, y2,figure;
-	while (1) {
+	int x1, x2, y1, y2,figure, f1 = 0,f2 = 0,f3 = 0,f4 = 0,f5 = 0;
 		printf("Enter x and y for first figure: ");
 		scanf_s("%d %d", &x1, &y1);
 		printf("Enter x and y for second figure: ");
@@ -12,163 +11,123 @@ int main() {
 
 		printf("Select your hero:\n 1-Ferz\n 2-King\n 3-Ladia\n 4-Elephant\n 5-Horse\n ");
 		scanf_s("%d", &figure);
-		
+
+		if ((x1 == x2 || y1 == y2) || (abs(x2 - x1) == abs(y2 - y1))) {
+			f1 = 1;
+		}
+		if ((abs(x2 - x1) == 1 && abs(y2 - y1) <= 1) || (abs(y2 - y1) == 1 && abs(x2 - x1) <= 1)) {
+			f2 = 1;
+		}
+		if (abs(x2 - x1) == abs(y2 - y1)) {
+			f3 = 1;
+		}
+		if (x1 == x2 || y1 == y2) {
+			f4 = 1;
+		}
+		if (((abs(x2 - x1) == 3) && (abs(y2 - y1) == 1)) || ((abs(y2 - y1) == 3) && (abs(x2 - x1) == 1))) {
+			f5 = 1;
+		}
 		switch (figure) {
 		case 1:
-			printf("YOUR HERO IS FERZ\n");
-			if ((x1 == x2 || y1 == y2) || (abs(x2 - x1) == abs(y2 - y1))) {
-				printf("Mozhet\n");
+			if (f1 == 1) {
+				printf("Ferz mozhet\n");
 			}
 			else {
-				printf("nemozhet\n");
+				if (f2 == 1) {
+					printf("King mozhet\n");
+				}
+				if (f3 == 1) {
+					printf("Ladia mozhet\n");
+				}
+				if (f4 == 1) {
+					printf("Elephant mozhet\n");
+				}
+				if (f5 == 1) {
+					printf("Horse mozhet\n");
+				}
 			}
 			break;
 		case 2:
-			printf("YOUR HERO IS KING\n");
-			if ((abs(x2 - x1) == 1 && abs(y2 - y1) <= 1) || (abs(y2 - y1) == 1 && abs(x2 - x1) <= 1)) {
-				printf("MOZHET\n");
+			if (f2 == 1) {
+				printf("King mozhet\n");
 			}
 			else {
-				printf("NEMOZHET\n");
+				if (f1 == 1) {
+					printf("Ferz mozhet\n");
+				}
+				if (f3 == 1) {
+					printf("Ladia mozhet\n");
+				}
+				if (f4 == 1) {
+					printf("Elephant mozhet\n");
+				}
+				if (f5 == 1) {
+					printf("Horse mozhet\n");
+				}
 			}
 			break;
 		case 3:
-			printf("YOUR HERO IS LADIA\n");
-			if (abs(x2 - x1) == abs(y2 - y1)) {
-				printf("MOZHET\n");
+			if (f3 == 1) {
+				printf("Ladia mozhet\n");
+				break;
 			}
 			else {
-				printf("NE MOZHET\n");
+				if (f2 == 1) {
+					printf("King mozhet\n");
+				}
+				if (f1 == 1) {
+					printf("Ferz mozhet\n");
+				}
+				if (f4 == 1) {
+					printf("Elephant mozhet\n");
+				}
+				if (f5 == 1) {
+					printf("Horse mozhet\n");
+				}
 			}
 			break;
 		case 4:
-			printf("YOUR HERO IS ELEPHANT\n");
-			if (x1 == x2 || y1 == y2) {
-				printf("MOZHET\n");
+			if (f4 == 1) {
+				printf("Elephant mozhet\n");
+				break;
 			}
 			else {
-				printf("NE MOZHET\n");
+				if (f2 == 1) {
+					printf("King mozhet\n");
+				}
+				if (f3 == 1) {
+					printf("Ladia mozhet\n");
+				}
+				if (f1 == 1) {
+					printf("Ferz mozhet\n");
+				}
+				if (f5 == 1) {
+					printf("Horse mozhet\n");
+				}
 			}
 			break;
 		case 5:
-			printf("YOUR HERO IS HORSE\n");
-			if (((abs(x2 - x1) == 3) && (abs(y2 - y1) == 1)) || ((abs(y2 - y1) == 3) && (abs(x2 - x1) == 1))) {
-				printf("MOZHET\n");
+			if (f5 == 1) {
+				printf("Horse mozhet\n");
+				break;
 			}
 			else {
-				printf("NEMOZHET\n");
+				if (f2 == 1) {
+					printf("King mozhet\n");
+				}
+				if (f3 == 1) {
+					printf("Ladia mozhet\n");
+				}
+				if (f4 == 1) {
+					printf("Elephant mozhet\n");
+				}
+				if (f1 == 1) {
+					printf("Ferz mozhet\n");
+				}
 			}
 			break;
 		default:
 			break;
 		}
-		/*printf("Enter x and y for first figure: ");
-		scanf_s("%d %d", &x1, &y1);
-		printf("Enter x and y for second figure: ");
-		scanf_s("%d %d", &x2, &y2);
-
-		printf("Select your hero:\n 1-Ferz\n 2-King\n 3-Ladia\n 4-Elephant\n 5-Horse\n ");
-		scanf_s("%d", &figure);
-		if (figure == 1) {
-			printf("YOUR HERO IS FERZ\n");
-			if ((x1 == x2 || y1 == y2) || (abs(x2 - x1) == abs(y2 - y1))) {
-				printf("Mozhet\n");
-			}
-			else {
-				printf("nemozhet\n");
-			}
-		}
-		else if (figure == 2) {
-			printf("YOUR HERO IS KING\n");
-			if ((abs(x2 - x1) == 1 && abs(y2 - y1) <= 1) || (abs(y2 - y1) == 1 && abs(x2 - x1) <= 1)) {
-				printf("MOZHET\n");
-			}
-			else {
-				printf("NEMOZHET\n");
-			}
-		}
-		else if (figure == 3) {
-			printf("YOUR HERO IS LADIA\n");
-			if (abs(x2 - x1) == abs(y2 - y1)) {
-				printf("MOZHET\n");
-			}
-			else {
-				printf("NE MOZHET\n");
-			}
-
-		}
-		else if (figure == 4) {
-			printf("YOUR HERO IS ELEPHANT\n");
-			if (x1 == x2 || y1 == y2) {
-				printf("MOZHET\n");
-			}
-			else {
-				printf("NE MOZHET\n");
-			}
-		}
-		else if (figure == 5) {
-			printf("YOUR HERO IS HORSE\n");
-			if (((abs(x2 - x1) == 3) && (abs(y2 - y1) == 1)) || ((abs(y2 - y1) == 3) && (abs(x2 - x1) == 1))) {
-				printf("MOZHET\n");
-			}
-			else {
-				printf("NEMOZHET\n");
-			}
-		}*/
+		return 0;
 	}
-	return 0;
-}
-
-/*void turn(int number, int x1, int y1, int x2, int y2) {
-	int x2_x1 = x2 - x1;
-	int y2_y1 = y2 - y1;
-	switch (number){
-	case 1:
-		printf("YOUR HERO IS FERZ\n");
-		if ((x1 == x2 || y1 == y2) || (abs(x2 - x1) == abs(y2 - y1))) {
-			printf("Mozhet\n");
-		}
-		else {
-			printf("nemozhet\n");
-		}
-		break;
-	case 2:
-		printf("YOUR HERO IS KING\n");
-		if ((abs(x2 - x1) == 1 && abs(y2 - y1) <= 1) || (abs(y2 - y1) == 1 && abs(x2 - x1) <= 1)) {
-			printf("MOZHET\n");
-		}
-		else {
-			printf("NEMOZHET\n");
-		}
-		break;
-	case 3:
-		printf("YOUR HERO IS LADIA\n");
-		if (abs(x2 - x1) == abs(y2 - y1)) {
-			printf("MOZHET\n");
-		}
-		else {
-			printf("NE MOZHET\n");
-		}
-		break;
-	case 4:
-		printf("YOUR HERO IS ELEPHANT\n");
-		if (x1 == x2 || y1 == y2) {
-			printf("MOZHET\n");
-		}
-		else {
-			printf("NE MOZHET\n");
-		}
-		break;
-	case 5:
-		printf("YOUR HERO IS HORSE\n");
-		if (((abs(x2 - x1) == 3) && (abs(y2 - y1) == 1)) || ((abs(y2 - y1) == 3) && (abs(x2 - x1) == 1))) {
-			printf("MOZHET\n");
-		}
-		else {
-			printf("NEMOZHET\n");
-		}
-		break;
-	default:
-		break;
-	}
-}*/
