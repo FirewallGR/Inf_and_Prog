ee6b5e32c614d9de338103cf5004c441902fe75e
FireCitrus <qwert.ting@bk.ru>
2021-09-24 09:47:34 +0300

    upgraded solution Task2

--- CWTasks/Task2CW/main.c
+++ CWTasks/Task2CW/main.c
@@ -2,29 +2,29 @@
 
 int main() {
 	int price;
-	int m1 = 1, m2 = 5, m3 = 10, m4 = 25, k = 0;
+	int m1 = 1, m2 = 5, m3 = 10, m4 = 25, k1 = 0, k2 = 0, k3 = 0, k4 = 0;
 
 	printf("Set the price Witcher ");
 	scanf_s("%d", &price);
 
 	while (price > 0) {
 		while (price >= m4) {
-			k++;
+			k4++;
 			price = price - m4;
 		}
 		while (price >= m3) {
-			k++;
+			k3++;
 			price = price - m3;
 		}
 		while (price >= m2) {
-			k++;
+			k2++;
 			price = price - m2;
 		}
 		while (price >= m1) {
-			k++;
+			k1++;
 			price = price -m1;
 		}
 	}
-	printf("%d", k);
+	printf("%d %d %d %d", k4,k3,k2,k1);
 	return 0;
 }
