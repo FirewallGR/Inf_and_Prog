1b8b67e305a855236429f6ceba97af8ff0d188c0
Goncov <qwert.ting@bk.ru>
2021-09-26 23:41:57 +0400

    Debugged first variant of Task, added second variant

---
+++ HWTasks/Task2/main.c
@@ -0,0 +1,72 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+
+int main() {
+	int Variant = 0, HNumber = 0, CNumber = 0, accuracy = 0, win = 0, count = 0, multiplier = 1000;
+	char comparison;
+	while (accuracy != 1) {
+		printf("Please, enter variant of game(1 or 2): ");
+		scanf_s("%d", &Variant);
+
+		switch (Variant)
+		{
+		case 1:
+			srand(time(NULL));
+			CNumber = rand() % 1000 + 1;
+			printf("%d\n", CNumber);
+			printf("Computer generated random number\n Your turn\n Guess the number\n");
+			while (win != 1) {
+				scanf_s("%d", &HNumber);
+				if (HNumber > CNumber) {
+					printf("The hidden number is less\n");
+					count++;
+				}
+				if (HNumber < CNumber) {
+					printf("The hidden number is greater\n");
+					count++;
+				}
+				if (HNumber == CNumber) {
+					printf("You got this\n");
+					count++;
+					win = 1;
+				}
+			}
+			accuracy = 1;
+			break;
+		case 2:
+			scanf_s("%d", &HNumber);
+			while (win != 1) {
+				srand(time(NULL));
+				CNumber = rand() % multiplier + 1;
+				printf("Computer guess that number is %d\n Is it true?\n", CNumber);
+				scanf_s("%c", &comparison);
+				if (comparison == '>') {
+					if (multiplier < HNumber) {
+						multiplier++;
+					}
+					count++;
+				}
+				else if (comparison == '<') {
+					multiplier--;
+					count++;
+				}
+				else if (comparison == '=') {
+					printf("Game over\n Computer is right\n");
+					count++;
+					win = 1;
+				}
+			}
+			accuracy = 1;
+			break;
+		default:
+			printf("You wrote wrong variant\n");
+			break;
+		}
+	}
+	printf("%d\n", count);
+
+	return 0;
+}
