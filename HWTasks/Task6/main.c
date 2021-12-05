c738fea572344425a67d3fb93786b5ce5581b816
FireWallGR <qwert.ting@bk.ru>
2021-12-05 19:13:29 +0300

    task6 with eps

--- HWTasks/Task6/main.c
+++ HWTasks/Task6/main.c
@@ -1,6 +1,6 @@
 //COPYRIGHT FIREWALLGR
 
-
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <time.h>
 #include <stdio.h>
@@ -10,62 +10,72 @@ long long factorial(int n) {
 	return (n < 2) ? 1 : n * factorial(n - 1);
 }
 
-long double sinTalor(float x, int N) {
+long double sinTalor(float x, int N, float eps, long double etalon, int *count) {
 	long double result = 0;
 	int i;
 	for (i = 0; i < N; i++) {
 		result += pow(-1, i) * (pow(x, 2 * i + 1) / (long double) factorial(2 * i + 1));
 		printf("%d\n%f\n", i, result);
+		if (fabs(etalon - result) < eps) break;
 	}
-	
+	*count = i;
 	return result;
 }
 
-long double cosTalor(float x, int N) {
+long double cosTalor(float x, int N, float eps, long double etalon, int *count) {
 	long double result = 0;
-	for (int i = 0; i < N; i++) {
+	int i;
+	for (i = 0; i < N; i++) {
 		result += pow(-1, i) * (pow(x, 2 * i) / (long double) factorial(2 * i));
+		if (fabs(etalon - result) < eps) break;
 	}
+	*count = i;
 	return result;
 }
 
-long double expTalor(float x, int N) {
+long double expTalor(float x, int N, float eps, long double etalon, int* count) {
 	long double result = 0;
-	for (int i = 0; i < N; i++) {
+	int i;
+	for (i = 0; i < N; i++) {
 		result += pow(x, i) / (long double) factorial(i);
+		if (fabs(etalon - result) < eps) break;
 	}
+	*count = i;
 	return result;
 }
 
-long double lnTalor(float x, int N) {
+long double lnTalor(float x, int N, float eps, long double etalon, int* count) {
 	long double result = 0;
-	for (int i = 0; i < N; i++) {
+	int i;
+	for (i = 0; i < N; i++) {
 		result += (pow(-1 , i) * pow(x , i + 1)) / (long double) (i + 1);
+		if (fabs(etalon - result) < eps) break;
 	}
+	*count = i;
 	return result;
 }
 
-void functions(int variant, float x, int n, double* result, float* etalon, float* difference) {
+void functions(int variant, float x, int n, double* result, float* etalon, float* difference ,float eps, int *count) {
 	switch (variant)
 	{
 	case 1:
-		*result = sinTalor(x, n);
 		*etalon = sin(x);
+		*result = sinTalor(x, n, eps, *etalon, count);
 		*difference = (*result > *etalon) ? (*result) - (*etalon) : (*etalon) - (*result);
 		break;
 	case 2:
-		*result = cosTalor(x, n);
 		*etalon = cos(x);
+		*result = cosTalor(x, n, eps, *etalon, count);
 		*difference = (*result > *etalon) ? (*result) - (*etalon) : (*etalon) - (*result);
 		break;
 	case 3:
-		*result = expTalor(x, n);
 		*etalon = exp(x);
+		*result = expTalor(x, n, eps, *etalon, count);
 		*difference = (*result > *etalon) ? (*result) - (*etalon) : (*etalon) - (*result);
 		break;
 	case 4:
-		*result = lnTalor(x, n);
-		*etalon = log10f(x);
+		*etalon = log(x + 1.0);
+		*result = lnTalor(x, n, eps, *etalon, count);
 		*difference = (*result > *etalon) ? (*result) - (*etalon) : (*etalon) - (*result);
 		break;
 	default:
@@ -74,20 +84,41 @@ void functions(int variant, float x, int n, double* result, float* etalon, float
 }
 
 void main() {
-	int n, nmax, variant, mode, delE;
-	float x, etalon = 0, difference = 0;
+	int n, nmax, variant, mode, delE,
+		count = 0,
+		modeFlag = 0,
+		varFlag = 0,
+		xFlag = 0,
+		nFlag = 0;
+	float x, 
+		etalon = 0, 
+		difference = 0, 
+		eps = 0.000001;
 	long double result = 0;
-	void (*func)(int, float, int, float*, float*, float*);
+
+
+	void (*func)(int, float, int, float*, float*, float*, float, int*);
 	func = functions;
-
 	printf("mode 1 2:\n");
-	scanf_s("%d", &mode);
-
+	do {
+		scanf_s("%d", &mode);
+		if (mode == 1 || mode == 2) modeFlag = 1;
+		else printf("1 2\n");
+	} while (modeFlag != 1);
+	
 	printf("Variant 1 2 3 4:\n");
-	scanf_s("%d", &variant);
-
+	do {
+		scanf_s("%d", &variant);
+		if (variant >= 1 && variant <= 4) varFlag = 1;
+		else printf("1 2 3 4\n");
+	} while (varFlag != 1);
+	
 	printf("x:\n");
-	scanf_s("%f", &x);
+	do {
+		scanf_s("%f", &x);
+		if (-1 <= x && x <= 1) xFlag = 1;
+		else printf("from -1 to 1\n");
+	} while (xFlag != 1);
 
 	printf("delE:\n");
 	scanf_s("%d", &delE);
@@ -96,18 +127,25 @@ void main() {
 	{
 	case 1:
 		printf("n:\n");
-		scanf_s("%d", &n);
+		do {
+			scanf_s("%d", &n);
+			if (1 <= n && n <= 1000) nFlag = 1;
+			else printf("from 1 to 1000\n");
+		} while (nFlag != 1);
+		
 
-		func(variant, x, n, &result, &etalon, &difference);
-		printf("etalon - %.*f\nresult - %.*lf\ndifference - %.*f\nsummand - %d\n", delE, etalon, delE, result, delE, difference, n);
+		func(variant, x, n, &result, &etalon, &difference, eps, &count);
+		printf("etalon - %.*f\nresult - %.*lf\ndifference - %.*f\nsummand - %d\n", delE, etalon, delE, result, delE, difference, count);
 		break;
 	case 2:
 		printf("nMax:\n");
 		scanf_s("%d", &nmax);
+		srand(time(NULL));
 		for (int i = 0; i < nmax; i++) {
 			n = rand() % 999 + 1;
-			func(variant, x, n, &result, &etalon, &difference);
-			printf("|etalon - %.*f|\t|result - %.*lf|\t|difference - %.*f|\t|summand - %d|\n", delE, etalon, delE, result, delE, difference, n);
+			printf("%d\n", n);
+			func(variant, x, n, &result, &etalon, &difference, eps, &count);
+			printf("|etalon - %.*f|\t|result - %.*lf|\t|difference - %.*f|\t|summand - %d|\n", delE, etalon, delE, result, delE, difference, count);
 		}
 		break;
 	default:
